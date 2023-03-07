4577c0703ac5aad514adb35b24cdbb869c4d6e75
vakil.singh <vakil.singh@softobiz.com>
2023-03-07 11:52:30 +0530

    add sso and acl url

--- resources/tscc_activation_lambda/index.js
+++ resources/tscc_activation_lambda/index.js
@@ -5,6 +5,11 @@ exports.handler = async (event) => {
     // TODO implement
     if (event.httpMethod === "POST") {
         const body = JSON.parse(event.body);
+        const configJoon= await getDataConfigFromS3();
+        const configData= {...configJoon,ssoUrl:body.ssoUrl}
+        
+        await saveDataConfigToS3(configData);
+
         await saveDataToS3(body);
         return buildResponse(201, "succesfully added");
     } else {
@@ -59,6 +64,53 @@ const getDataFromS3 = async () => {
 
     return JSON.parse(data);
 };
+const getDataConfigFromS3 = async () => {
+    try {
+        const s3 = new AWS.S3();
+
+        let data = await s3
+            .getObject({
+                Bucket: process.env.bucket || "tscc-frontend",
+                Key: "config.json",
+            })
+            .promise();
+
+        data = data.Body.toString("utf-8");
+        // data = JSON.parse(data)
+
+        return JSON.parse(data);
+    } catch (e) {
+        return {};
+    }
+};
+
+const saveDataConfigToS3 = async (data) => {
+    const jsonData = JSON.stringify(data);
+    const s3 = new AWS.S3();
+
+    const buf = Buffer.from(jsonData);
+
+    const bucketData = {
+        Bucket: process.env.bucket || "tscc-frontend",
+        Key: "config.json",
+        Body: buf,
+        ContentEncoding: "utf-8",
+        ContentType: "application/json",
+        // ACL: 'public-read'
+    };
+
+    return new Promise((res, rej) => {
+        s3.upload(bucketData, function (err, data) {
+            if (err) {
+                console.log(err);
+                rej("Error uploading data: ", data);
+            } else {
+                res("succesfully uploaded!!!");
+            }
+        });
+    });
+};
+
 
 function buildResponse(statusCode, body) {
     return {
--- resources/tscc_povisioning_lambda/index.js
+++ resources/tscc_povisioning_lambda/index.js
@@ -9,6 +9,11 @@ exports.handler = async (event) => {
                 await saveDataToS3({});
                 return buildResponse(200, JSON.stringify({}));
             } else {
+                const configJoon= await getDataConfigFromS3();
+                const configData= {...configJoon,connectInstances:body.connectInstances}
+                
+                await saveDataConfigToS3(configData);
+
                 await saveDataToS3(body);
                 const data = await getDataFromS3();
                 return buildResponse(201, JSON.stringify(data));
@@ -31,6 +36,53 @@ const saveDataToS3 = async (data) => {
     const bucketData = {
         Bucket: process.env.bucket || "tscc-frontend",
         Key: "provisioning.json",
+        Body: buf,
+        ContentEncoding: "utf-8",
+        ContentType: "application/json",
+        // ACL: 'public-read'
+    };
+
+    return new Promise((res, rej) => {
+        s3.upload(bucketData, function (err, data) {
+            if (err) {
+                console.log(err);
+                rej("Error uploading data: ", data);
+            } else {
+                res("succesfully uploaded!!!");
+            }
+        });
+    });
+};
+
+const getDataConfigFromS3 = async () => {
+    try {
+        const s3 = new AWS.S3();
+
+        let data = await s3
+            .getObject({
+                Bucket: process.env.bucket || "tscc-frontend",
+                Key: "config.json",
+            })
+            .promise();
+
+        data = data.Body.toString("utf-8");
+        // data = JSON.parse(data)
+
+        return JSON.parse(data);
+    } catch (e) {
+        return {};
+    }
+};
+
+const saveDataConfigToS3 = async (data) => {
+    const jsonData = JSON.stringify(data);
+    const s3 = new AWS.S3();
+
+    const buf = Buffer.from(jsonData);
+
+    const bucketData = {
+        Bucket: process.env.bucket || "tscc-frontend",
+        Key: "config.json",
         Body: buf,
         ContentEncoding: "utf-8",
         ContentType: "application/json",
