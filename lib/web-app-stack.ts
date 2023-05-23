8b0ac34ec0a733e2a79585fe76b545c8ba38a2ca
Amit Kumar <amit.thakur@softobiz.com>
2023-05-23 10:14:03 +0530

    solve bug s3putpolicyaccesdenied

--- lib/web-app-stack.ts
+++ lib/web-app-stack.ts
@@ -5,6 +5,7 @@ import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import ResourcesName from "./constants";
 
+
 const bucketName = ResourcesName.BUCKET;
 
 interface WepAppStackProps extends cdk.StackProps {
@@ -23,12 +24,38 @@ export class WepAppStack extends cdk.Stack {
          */
         const tscc_web_app_bucket = new s3.Bucket(this, bucketName, {
             publicReadAccess: true,
+
+
+            ///////////////////////////////////////////////////
+            blockPublicAccess: new s3.BlockPublicAccess({
+                blockPublicAcls: false,
+                ignorePublicAcls: false,
+                blockPublicPolicy: false,
+                restrictPublicBuckets: false,
+              }),
+            /////////////////////////////////////////////////////  
             bucketName: bucketName,
             websiteIndexDocument: 'index.html',
             websiteErrorDocument: 'index.html',
             removalPolicy: cdk.RemovalPolicy.DESTROY,
             autoDeleteObjects: true,
         });
+
+
+      // Add bucket policy to allow public read access
+      tscc_web_app_bucket.addToResourcePolicy(
+        new cdk.aws_iam.PolicyStatement({
+            actions: [
+                's3:*',
+                's3:GetObject',
+                's3:ListBucket',
+                's3:PutBucketPolicy',
+            ],
+            resources: [tscc_web_app_bucket.bucketArn, `${tscc_web_app_bucket.bucketArn}/*`],
+            principals: [new cdk.aws_iam.AnyPrincipal()],
+        })
+    );/////////////////////////////////////////////////////
+  
         // web app deployment
         new s3Deployment.BucketDeployment(this, ResourcesName.WEB_APP, {
             sources: [s3Deployment.Source.asset("./resources/build")],
